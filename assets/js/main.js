9d760d810e521f8d60a49649afd28687ea2fdbbc
Taha <dhaileytaha@gmail.com>
2020-06-01 01:21:28 +0800

    added new feature

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,3 +59,11 @@ $("#zk_btn").click(function(){
 
     })
 })    
+
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 600, 'linear');
+  });
+});
